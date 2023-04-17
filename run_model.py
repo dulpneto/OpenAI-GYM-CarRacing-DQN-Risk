f0ab488c840500f730cae627beebd24bdc8f1b89
Eduardo <eduardo.lopes@tail.digital>
2023-04-17 10:42:13 -0300

    make run model works same frames as train

--- run_model.py
+++ run_model.py
@@ -5,6 +5,8 @@ from car_river_crossing import CarRiverCrossing
 from common_functions import generate_state_frame_stack_from_queue
 from common_functions import process_state_image
 
+SKIP_FRAMES                   = 3
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Play CarRacing by the trained model.')
     parser.add_argument('-m', '--model', required=True, help='The `.h5` file of the trained model.')
@@ -15,10 +17,14 @@ if __name__ == '__main__':
     train_model = args.model
     play_episodes = args.episodes
 
+    play_area = 200
+    zoom = 1.8
+
     if args.render:
-        env = CarRiverCrossing(render_mode='human')
+        env = CarRiverCrossing(render_mode='human', play_field_area=play_area, zoom=zoom)
     else:
-        env = CarRiverCrossing()
+        env = CarRiverCrossing(play_field_area=play_area, zoom=zoom)
+
     # Set epsilon to 0 to ensure all actions are instructed by the agent
     agent = CarRacingDQNAgent(epsilon=0, lamb=args.lamb)
     agent.load(train_model)
@@ -37,8 +43,15 @@ if __name__ == '__main__':
             current_state_frame_stack = generate_state_frame_stack_from_queue(state_frame_stack_queue)
             action = agent.act(current_state_frame_stack)
             next_state, reward, terminated, truncated, info = env.step(action)
-            next_state = process_state_image(next_state)
 
+            reward = 0
+            for _ in range(SKIP_FRAMES + 1):
+                next_state, r, terminated, truncated, info = env.step(action)
+                reward += r
+                if terminated or truncated:
+                    break
+
+            next_state = process_state_image(next_state)
             done = terminated
 
             init_state = next_state
